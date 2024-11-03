f3a7e80d9ceea6b989c49f298df55f0643927f82
cbas30are <eduardoarevalo45@gmail.com>
2024-11-03 21:58:10 +1100

    Add files via upload

---
+++ scripts/policy.js
@@ -0,0 +1,9 @@
+// Function to display a privacy policy alert
+function showPrivacyPolicy() {
+    alert("Privacy Policy: We value your privacy and ensure your data is protected. For more details, contact our support.");
+}
+
+// Display Privacy Policy on page load
+window.onload = function() {
+    showPrivacyPolicy();
+};
